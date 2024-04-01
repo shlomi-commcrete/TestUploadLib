af584b5074d076031ef81bb891876293125f3079
Shlomo <Shlomo.A@commcrete.co.il>
2024-04-01 21:22:39 +0300

    changed maven

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    id("maven-publish")
 }
 
 android {
@@ -35,4 +37,18 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+}
+
+afterEvaluate{
+    publishing{
+        publications{
+            create<MavenPublication>("release") {
+                from(components["release"])
+
+                groupId = "com.commcre.testlib"
+                artifactId = "testlib-upload"
+                version = "1.0.0"
+            }
+        }
+    }
 }
